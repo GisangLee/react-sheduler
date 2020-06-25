5e15c910853888ddffd62c29fad13afb6007ec72
DaeLee <masterkorea01@naver.com>
2020-06-25 09:39:13 +0900

    moment.js

--- src/sheduler.js
+++ src/sheduler.js
@@ -6,32 +6,46 @@ import {
   DayView,
   WeekView,
   Appointments,
+  AppointmentTooltip,
+  AppointmentForm,
 } from "@devexpress/dx-react-scheduler-material-ui";
+import moment from "moment";
+import styled from "styled-components";
 
-const currentDate = "2020-06-23";
+// const currentDate = "2020-06-23";
+
+//moment
+const date = moment().format("YYYY-MM-DD");
+
 const schedulerData = [
   {
-    startDate: "2020-06-23T09:45",
-    endDate: "2020-06-23T11:00",
-    title: "Meeting",
-  },
-  {
-    startDate: "2020-06-23T12:00",
-    endDate: "2020-06-23T13:30",
+    startDate: moment().add(2, "hours").format("YYYY-MM-DDThh:mm"),
+    endDate: moment().add(3, "hours").format("YYYY-MM-DDThh:mm"),
     title: "Go to a gym",
   },
 ];
 
 function MySheculer() {
+  console.log(date);
   return (
-    <Paper>
-      <Scheduler data={schedulerData}>
-        <ViewState currentDate={currentDate} />
-        <DayView startDayHour={9} endDayHour={24} />
-        <Appointments />
-      </Scheduler>
-    </Paper>
+    <>
+      <Container>
+        <Paper>
+          <Scheduler data={schedulerData} height={660}>
+            <WeekView startDayHour={8} endDayHour={24} />
+            <Appointments />
+            <AppointmentTooltip showCloseButton showOpenButton />
+            <AppointmentForm />
+          </Scheduler>
+        </Paper>
+      </Container>
+    </>
   );
 }
+
+const Container = styled.div`
+  width: 50%;
+  margin: 100px auto;
+`;
 
 export default MySheculer;
