09c1dbe88b85185aeca956e36527fe8d284e29ac
DaeLee <masterkorea01@naver.com>
2020-06-27 10:23:16 +0900

    schedule

--- src/sheduler.js
+++ src/sheduler.js
@@ -1,52 +1,97 @@
 import React from "react";
+import moment from "moment";
+import "moment/locale/ko";
+import styled from "styled-components";
 import Paper from "@material-ui/core/Paper";
 import { ViewState } from "@devexpress/dx-react-scheduler";
 import {
   Scheduler,
-  DayView,
   WeekView,
   Appointments,
   AppointmentTooltip,
   AppointmentForm,
 } from "@devexpress/dx-react-scheduler-material-ui";
-import moment from "moment";
-import styled from "styled-components";
+import { useState, useEffect } from "react";
 
-// const currentDate = "2020-06-23";
+moment.lang("ko", {
+  weekdays: ["월요일", "화요일", "수요일", "목요일", "금요일"],
+  weekdaysShort: ["월", "화", "수", "목", "금"],
+});
 
-//moment
-const date = moment().format("YYYY-MM-DD");
+const currentDate = moment().format("YYYY-MM-DD");
 
-const schedulerData = [
+const data = [
   {
-    startDate: moment().add(2, "hours").format("YYYY-MM-DDThh:mm"),
-    endDate: moment().add(3, "hours").format("YYYY-MM-DDThh:mm"),
-    title: "Go to a gym",
+    title: "전산개론 시험",
+    startDate: moment("2020-06-26T17:00"),
+    endDate: moment("2020-06-26T17:00").add(1, "hours"),
+    allDay: false,
+  },
+  {
+    title: "디지털공학개론 시험",
+    startDate: moment("2020-06-26T14:00"),
+    endDate: moment("2020-06-26T14:00").add(1, "hours"),
+    allDay: false,
+  },
+  {
+    title: "유티쿼터스 시험",
+    startDate: moment("2020-06-26T11:00"),
+    endDate: moment("2020-06-26T11:00").add(2, "hours"),
+    allDay: false,
+  },
+  {
+    title: "PC활용 시험",
+    startDate: moment("2020-06-23T10:00"),
+    endDate: moment("2020-06-23T10:00").add(2, "hours"),
+    allDay: false,
+  },
+  {
+    title: "C언어 시험",
+    startDate: moment("2020-06-23T14:00"),
+    endDate: moment("2020-06-23T14:00").add(1, "hours"),
+    allDay: false,
+  },
+  {
+    title: "멀티미디어 시험",
+    startDate: moment("2020-06-22T12:00"),
+    endDate: moment("2020-06-22T12:00").add(1, "hours"),
+    allDay: false,
   },
 ];
 
-const defineTitle = () => "일정";
+const notToDisplay = [0, 6];
+
 function MySheculer() {
-  console.log(date);
+  const [state, setState] = useState();
+
+  useEffect(() => {
+    setState(data);
+  }, []);
+
+  console.log(state);
+
   return (
-    <>
-      <Container>
-        <Paper>
-          <Scheduler data={schedulerData} height={660}>
-            <WeekView startDayHour={8} endDayHour={24} />
-            <Appointments />
-            <AppointmentTooltip showCloseButton showOpenButton />
-            <AppointmentForm labelComponent={defineTitle} />
-          </Scheduler>
-        </Paper>
-      </Container>
-    </>
+    <Container>
+      <Paper>
+        <Scheduler data={data} height={660} locale={"ko-KR"} firstDayOfWeek={1}>
+          <ViewState defaultCurrentDate={currentDate} />
+          <WeekView
+            startDayHour={9}
+            endDayHour={19}
+            excludedDays={notToDisplay}
+          />
+          <Appointments />
+          <AppointmentTooltip showCloseButton showOpenButton />
+          <AppointmentForm />
+        </Scheduler>
+      </Paper>
+    </Container>
   );
 }
 
 const Container = styled.div`
-  width: 50%;
-  margin: 100px auto;
+  width: 60%;
+  margin: 0 auto;
 `;
 
 export default MySheculer;
